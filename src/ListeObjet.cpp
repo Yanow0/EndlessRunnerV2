29f882d57e441dd4fd608234bfb8ca086ce6dbc0
Valentin Gachod <valen.gachod@hotmail.fr>
2019-05-06 13:46:43 +0200

    Ajout liste d'objets

---
+++ src/ListeObjet.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <cstdlib>
+#include "ListeObjet.h"
+
+using namespace std;
+
+ListeObjet::ListeObjet()
+{
+    creation();
+}
+
+ListeObjet::~ListeObjet()
+{
+    vider();
+}
+
+int ListeObjet::nbObjet()
+{
+    return objets.size();
+}
+
+bool ListeObjet::listeVide()
+{
+    return objets.empty();
+}
+
+Objet* ListeObjet::getObjet(const int &i)
+{
+    return &objets[i];
+}
+
+void ListeObjet::ajouterObjet(const Objet o)
+{
+    objets.push_back(o);
+}
+
+void ListeObjet::supprimerEnTete()
+{
+    objets.erase(objets.begin());
+}
+
+bool ListeObjet::positionValide(const Objet o)
+{
+    return (listeVide()
+            || (o.pos->getX() > objets.back().pos->getX() + objets.back().taille->getLargeur() + 7)
+            || ((o.pos->getX() > objets.back().pos->getX() + objets.back().taille->getLargeur() + 2)
+                && ((o.pos->getY() > objets.back().pos->getY() + objets.back().taille->getHauteur() + 6)
+                    || (objets.back().pos->getY() > o.pos->getY() + o.taille->getHauteur() + 6))));
+}
+
+void ListeObjet::creation()
+{
+    Objet o = Objet();
+    if(positionValide(o)){
+        ajouterObjet(o);
+    }
+}
+
+void ListeObjet::deplacementAuto()
+{
+    creation();
+    for (int i=0; i<nbObjet(); i++) {
+        objets[i].deplacementAuto();
+    }
+    while (objets.front().pos->getX()+objets.front().taille->getLargeur() <= 0)
+        supprimerEnTete();
+}
+
+void ListeObjet::vider()
+{
+    objets.clear();
+}
---
+++ src/ListeObjet.h
@@ -0,0 +1,35 @@
+#ifndef LISTEOBJET_H
+#define LISTEOBJET_H
+
+#include <vector>
+#include "Objet.h"
+
+using namespace std;
+
+class ListeObjet
+{
+    public:
+        ListeObjet();
+        ~ListeObjet();
+        int nbObjet();
+        bool listeVide();
+        Objet* getObjet(const int &i);
+        void ajouterObjet(const Objet o);
+        void supprimerEnTete();
+
+        // tester si la position d'un nouvel objet est valide
+        // (pour ensuite l'insérer dans la liste d'objet)
+        bool positionValide(const Objet o);
+
+        // création aléatoire d'un objet valide
+        void creation();
+
+        // deplacement de tous les objets dans la liste
+        void deplacementAuto();
+
+        void vider();
+    private:
+        vector<Objet> objets;
+};
+
+#endif // LISTEOBJET_H
