79b50bc8eaaf67e52416339c25cfe221470202f9
Valentin Gachod <valen.gachod@hotmail.fr>
2019-05-07 01:03:19 +0200

    Ajout doc (manquera juste à lancer la commande pour générer automatiquement)

--- src/Terrain.h
+++ src/Terrain.h
@@ -11,24 +11,50 @@
 class Terrain {
 
 private:
-	int dimx;
-	int dimy;
+	int dimx; //!< Variable membre, entier représentant la dimension en x du terrain.
+	int dimy; //!< Variable membre, entier représentant la dimension en y du terrain.
 	char ter [100][100];
-    int plateforme;
+    int plateforme; //!< Variable membre, entier représentant la hauteur du sol.
 
 public:
-    //Constructeur Terrain
+    //! Constructeur de Terrain sans paramètre
+    /*!
+        Initialise les variables membre de Terrain
+    */
     Terrain();
 
-    //Destructeur Terrain
+    //Destructeur de Terrain
     ~Terrain();
 
+    //! Getter de dimX de Terrain
+    /*!
+        Retourne l'entier dimX de Terrain.
+    */
     int getDimX() const;
+
+    //! Getter de dimY de Terrain
+    /*!
+        Retourne l'entier dimY de Terrain.
+    */
     int getDimY() const;
+
+    //! Getter de platefrome de Terrain
+    /*!
+        Retourne l'entier plateforme de Terrain.
+    */
     int getPlateforme() const;
 
+    //! Getter de la position en x et y  de Terrain
+    /*!
+        Retourne les entiers x et y de Terrain.
+    */
     char getXY(const int &x, const int &y) const;
 
+   //! positionValide , fonction memebre de Terrain
+   /*!
+        Verifie si la position entree est correcte
+        \param x,y int, les positions x et y
+    */
     bool positionValide(const int &x, const int &y) const;
 };
 
